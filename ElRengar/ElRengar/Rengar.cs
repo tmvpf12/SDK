446dc3912707c988c71ef197311fe1510165edeb
jQuery <info@zavox.nl>
2015-06-29 08:28:48 +0200

    .

--- ElRengar/ElRengar/Rengar.cs
+++ ElRengar/ElRengar/Rengar.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ElRengar
+﻿namespace ElRengar
 {
-    internal class Rengar : Config.Menu
+    using ElRengar.Config;
+
+    internal class Rengar : Menu
     {
-      
+        
     }
 }
