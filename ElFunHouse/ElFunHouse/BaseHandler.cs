59727af56fe11b93398575b482e067559dcfa8e8
jQuery <info@zavox.nl>
2015-07-04 18:20:14 +0200

    Champions

--- ElFunHouse/ElFunHouse/BaseHandler.cs
+++ ElFunHouse/ElFunHouse/BaseHandler.cs
@@ -3,16 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using LeagueSharp;
-using LeagueSharp.SDK.Core.Events;
 
 namespace ElFunHouse
 {
-    class Program
+    class BaseHandler
     {
-        static void Main(string[] args)
-        {
 
-        }
     }
 }
