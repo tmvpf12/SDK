d86ce936b6247df418b3c180f4c644f31e8ab074
jQuery <info@zavox.nl>
2015-07-07 20:42:01 +0200

    version1

--- ElRengar/ElRengar/Properties/AssemblyInfo.cs
+++ ElRengar/ElRengar/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("224.3232.23232.1")]
-[assembly: AssemblyFileVersion("224.3232.23232.1")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
